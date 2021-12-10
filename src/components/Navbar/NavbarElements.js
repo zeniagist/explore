786500fd259ae2e6ffded90e988af3872cb8e0f7
Zenia Gist <zg@buildmidwestern.com>
2021-12-10 09:51:23 -0600

    updated dependency

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,5 +1,5 @@
 import { Link } from "gatsby"
-import { DiScala } from "react-icons/Di"
+import { DiScala } from "react-icons/di"
 import styled from "styled-components"
 
 export const Nav = styled.nav`
